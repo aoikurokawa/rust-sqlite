1f850b540802e1627e9db5d33f3f1a82c2a74104
aoikurokawa <ht.so.ik0102@gmail.com>
2023-12-23 11:03:11 +0900

    style: format

--- src/header.rs
+++ src/header.rs
@@ -1,17 +1,11 @@
 pub struct Header {
-
     pub page_size: usize,
-
 }
 
 impl Header {
-
     pub fn new(data: &[u8]) -> Self {
-
         let page_size = u16::from_be_bytes([data[16], data[17]]) as usize;
 
         Self { page_size }
-
     }
-
 }
