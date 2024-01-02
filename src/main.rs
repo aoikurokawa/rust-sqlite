348080d0c8d7ce151e4493543ed6a6f7dce4a89e
aoikurokawa <ht.so.ik0102@gmail.com>
2024-01-02 09:49:40 +0900

    feat: define main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::{collections::HashSet, time::Instant};
+
 use anyhow::{bail, Result};
-use rust_sqlite::{column::SerialValue, database::Database};
+use sqlite_starter_rust::{column::SerialValue, database::Database, sql::Sql};
 
 fn main() -> Result<()> {
+    let now = Instant::now();
     // Parse arguments
     let args = std::env::args().collect::<Vec<_>>();
     match args.len() {
@@ -10,124 +13,178 @@ fn main() -> Result<()> {
         _ => {}
     }
 
+    let file_path = &args[1];
+    let db = Database::read_file(file_path)?;
     // Parse command and act accordingly
     let command = &args[2];
     match command.as_str() {
         ".dbinfo" => {
-            let file_path = &args[1];
-
-            let db = Database::read_file(file_path)?;
             println!("database page size: {}", db.page_size());
 
-            println!("number of tables: {}", db.tables());
-        }
-        ".tables" => {
-            let file_path = &args[1];
-
-            let db = Database::read_file(file_path)?;
-            match db.pages.get(0) {
-                Some(first_page) => {
-                    eprintln!("cell offsets: {:?}", first_page.cell_offsets); // [3983, 3901, 3779]
-
-                    for i in 0..db.tables() {
-                        if let Ok(record) = first_page.read_cell(i) {
-                            eprintln!("{:?}", record.columns[0].data());
-                            match record.columns[0].data() {
-                                SerialValue::String(ref str) => {
-                                    if str != "table" {
-                                        continue;
-                                    }
-                                }
-                                _ => {}
-                            }
-
-                            eprintln!("{:?}", record.columns[2].data());
-                            let tbl_name = match record.columns[2].data() {
-                                SerialValue::String(ref str) => {
-                                    if str != "sqlite_sequence" {
-                                        continue;
-                                    }
-                                    str
-                                }
-                                _ => "",
-                            };
-
-                            eprintln!("{tbl_name}");
-                        };
-                    }
-                }
-                None => eprintln!("can not read first page"),
+            if let Some(first_page) = db.pages.get(0) {
+                println!("number of tables: {}", first_page.btree_header.ncells());
             }
         }
-        query if query.to_lowercase().starts_with("select") => {
-            let file_path = &args[1];
-            let sql_query = nom_sql::parse_query(query).expect("");
-            // let target_table = query.split(" ").last().expect("specify table name");
-
-            match sql_query {
-                nom_sql::SqlQuery::Select(select) => {
-                    let db = Database::read_file(file_path)?;
-                    if let Some(first_page) = db.pages.get(0) {
-                        for i in 0..db.tables() {
-                            if let Ok(record) = first_page.read_cell(i) {
-                                match record.columns[0].data() {
-                                    SerialValue::String(ref str) => {
-                                        if str != "table" {
-                                            continue;
-                                        }
-                                    }
-                                    _ => {}
-                                }
-
-                                match record.columns[2].data() {
-                                    SerialValue::String(str) => match str.as_str() {
-                                        "sqlite_sequence" => {
-                                            continue;
-                                        }
-                                        t_name => {
-                                            for table_name in &select.tables {
-                                                // println!("{:?}", target_table);
-                                                if table_name.name == t_name {
-                                                    match record.columns[3].data() {
-                                                        SerialValue::I8(num) => {
-                                                            // eprintln!("num: {num}");
-                                                            if let Some(page) =
-                                                                db.pages.get(*num as usize - 1)
-                                                            {
-                                                                let cell_len =
-                                                                    page.cell_offsets.len();
-                                                                println!("{:?}", cell_len);
-
-                                                                for i in 0..cell_len {
-                                                                    let record =
-                                                                        page.read_cell(i as u16)?;
-
-                                                                    println!(
-                                                                        "record: {:?}",
-                                                                        record.columns[1]
-                                                                    );
-                                                                }
-                                                            }
-                                                        }
-                                                        _ => {}
-                                                    }
-                                                }
-                                            }
-                                        }
-                                        _ => {}
-                                    },
-
-                                    _ => {}
+        ".tables" => match db.pages.get(0) {
+            Some(first_page) => {
+                let mut tables = String::new();
+                for i in 0..first_page.btree_header.ncells() {
+                    if let Ok((_, Some(record))) = first_page.read_cell(i) {
+                        match record.columns[0].data() {
+                            SerialValue::String(ref str) => {
+                                if str != "table" {
+                                    continue;
                                 }
                             }
+                            _ => {}
+                        }
 
-                            // tables.push_str(&format!("{} ", tbl_name));
+                        let tbl_name = match record.columns[2].data() {
+                            SerialValue::String(ref str) => {
+                                if str == "sqlite_sequence" {
+                                    continue;
+                                }
+                                &str
+                            }
+                            _ => "",
+                        };
+
+                        tables.push_str(&format!("{} ", tbl_name));
+                    };
+                }
+                println!("{tables}");
+            }
+            None => eprintln!("can not read first page"),
+        },
+        query if query.to_lowercase().starts_with("select count(*)") => {
+            let select_statement = Sql::from_str(query);
+
+            if let Some(first_page) = db.pages.get(0) {
+                for i in 0..first_page.btree_header.ncells() {
+                    if let Ok((_, Some(record))) = first_page.read_cell(i) {
+                        match record.columns[0].data() {
+                            SerialValue::String(ref str) => {
+                                if str != "table" {
+                                    continue;
+                                }
+                            }
+                            _ => {}
+                        }
+
+                        match record.columns[2].data() {
+                            SerialValue::String(str) => match str.as_str() {
+                                "sqlite_sequence" => {
+                                    continue;
+                                }
+                                t_name => {
+                                    if select_statement.tbl_name == t_name {
+                                        match record.columns[3].data() {
+                                            SerialValue::I8(num) => {
+                                                // eprintln!("num: {num}");
+                                                if let Some(page) = db.pages.get(*num as usize - 1)
+                                                {
+                                                    let cell_len = page.cell_offsets.len();
+                                                    println!("{:?}", cell_len);
+                                                }
+                                            }
+                                            _ => {}
+                                        }
+                                    }
+                                }
+                            },
+
+                            _ => {}
                         }
                     }
                 }
-                _ => todo!(),
             }
-            // println!("{tables}");
+        }
+        query if query.to_lowercase().starts_with("select") => {
+            let select_statement = Sql::from_str(query);
+
+            if let Some(first_page) = db.pages.get(0) {
+                for i in (0..first_page.btree_header.ncells()).rev() {
+                    match first_page.read_cell(i)? {
+                        (_, Some(record)) => {
+                            let mut rowids = HashSet::new();
+
+                            match record.columns[0].data() {
+                                SerialValue::String(str) => match str.as_str() {
+                                    "index" => {
+                                        let index_statement =
+                                            Sql::from_str(&record.columns[4].data().display());
+                                        if let SerialValue::I8(num) = record.columns[3].data() {
+                                            db.read_index(
+                                                *num as usize,
+                                                &index_statement,
+                                                &select_statement,
+                                                &mut rowids,
+                                            );
+                                        }
+                                        continue;
+                                    }
+                                    _ => {}
+                                },
+                                _ => {}
+                            }
+
+                            let mut rowids: Vec<i64> = rowids.into_iter().collect();
+                            rowids.sort_unstable();
+
+                            match record.columns[2].data() {
+                                SerialValue::String(str) => match str.as_str() {
+                                    "sqlite_sequence" => {
+                                        continue;
+                                    }
+                                    t_name => {
+                                        if select_statement.tbl_name == t_name {
+                                            match record.columns[3].data() {
+                                                SerialValue::I8(num) => {
+                                                    let create_statement = Sql::from_str(
+                                                        &record.columns[4].data().display(),
+                                                    );
+
+                                                    let fields = select_statement
+                                                        .get_fields(&create_statement);
+
+                                                    let mut row_set = HashSet::new();
+                                                    let mut rowid_set = HashSet::new();
+
+                                                    if rowids.is_empty() {
+                                                        db.read_table(
+                                                            *num as usize,
+                                                            &select_statement,
+                                                            fields,
+                                                            &mut row_set,
+                                                            &mut rowid_set,
+                                                        );
+                                                    } else {
+                                                        db.read_ids_from_table(
+                                                            *num as usize,
+                                                            &select_statement,
+                                                            fields,
+                                                            &mut row_set,
+                                                            &mut rowid_set,
+                                                            &rowids,
+                                                        );
+                                                    }
+
+                                                    row_set
+                                                        .iter()
+                                                        .for_each(|str| println!("{str}"));
+                                                }
+                                                _ => {}
+                                            }
+                                        }
+                                    }
+                                },
+                                _ => {}
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
