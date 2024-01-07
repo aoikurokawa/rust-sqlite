14c9a720c95d4b261f3b1dca43e90e1bd70fb18f
aoikurokawa <ht.so.ik0102@gmail.com>
2024-01-07 15:42:27 +0900

    test: select multiple field

--- tests/select.rs
+++ tests/select.rs
@@ -67,3 +67,20 @@ fn test_cli_select_single_field_sample_db() {
     assert!(output.status.success());
 }
 
+/*
+*
+* SELECT `field_name` FROM `table`
+*
+* **/
+#[test]
+fn test_cli_select_multiple_field_sample_db() {
+    let output = build_select_field_command("sample.db", "SELECT name, color FROM apples");
+    let stdout = String::from_utf8(output.stdout).expect("parse to String");
+    let expects = vec!["Granny Smith|Light Green", "Fuji|Red", "Honeycrisp|Blush Red", "Golden Delicious|Yellow"];
+
+    let outputs: Vec<&str> = stdout.lines().collect();
+
+    assert_eq!(expects, outputs);
+    assert!(output.status.success());
+}
+
