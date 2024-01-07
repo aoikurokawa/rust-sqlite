b869d1f38024c6b3f72d4aff87977391c8dc06f5
aoikurokawa <ht.so.ik0102@gmail.com>
2024-01-07 19:44:23 +0900

    fix: use if let

--- src/main.rs
+++ src/main.rs
@@ -113,87 +113,77 @@ fn main() -> anyhow::Result<()> {
 
                     if let Some(first_page) = db.pages.get(0) {
                         for i in (0..first_page.btree_header.ncells()).rev() {
-                            match first_page.read_cell(i)? {
-                                (_, Some(record)) => {
-                                    let mut rowids = HashSet::new();
+                            if let (_, Some(record)) = first_page.read_cell(i)? {
+                                let mut rowids = HashSet::new();
 
-                                    match record.columns[0].data() {
-                                        SerialValue::String(str) => match str.as_str() {
-                                            "index" => {
-                                                let index_statement = Sql::from_str(
-                                                    &record.columns[4].data().display(),
-                                                )?;
-                                                if let SerialValue::I8(num) =
-                                                    record.columns[3].data()
-                                                {
-                                                    db.read_index(
-                                                        *num as usize,
-                                                        &index_statement,
-                                                        &select_statement,
-                                                        &mut rowids,
-                                                    );
-                                                }
-                                                continue;
-                                            }
-                                            _ => {}
-                                        },
-                                        _ => {}
-                                    }
-
-                                    let mut rowids: Vec<i64> = rowids.into_iter().collect();
-                                    rowids.sort_unstable();
-
-                                    match record.columns[2].data() {
-                                        SerialValue::String(str) => match str.as_str() {
-                                            "sqlite_sequence" => {
-                                                continue;
-                                            }
-                                            t_name => {
-                                                if select_statement.tbl_name == t_name {
-                                                    match record.columns[3].data() {
-                                                        SerialValue::I8(num) => {
-                                                            let create_statement = Sql::from_str(
-                                                                &record.columns[4].data().display(),
-                                                            )?;
-
-                                                            let fields = select_statement
-                                                                .get_fields(&create_statement);
-
-                                                            let mut row_set = HashSet::new();
-                                                            let mut rowid_set = HashSet::new();
-
-                                                            if rowids.is_empty() {
-                                                                db.read_table(
-                                                                    *num as usize,
-                                                                    &select_statement,
-                                                                    fields,
-                                                                    &mut row_set,
-                                                                    &mut rowid_set,
-                                                                );
-                                                            } else {
-                                                                db.read_ids_from_table(
-                                                                    *num as usize,
-                                                                    &select_statement,
-                                                                    fields,
-                                                                    &mut row_set,
-                                                                    &mut rowid_set,
-                                                                    &rowids,
-                                                                );
-                                                            }
-
-                                                            row_set
-                                                                .iter()
-                                                                .for_each(|str| println!("{str}"));
-                                                        }
-                                                        _ => {}
-                                                    }
-                                                }
-                                            }
-                                        },
-                                        _ => {}
+                                if let SerialValue::String(str) = record.columns[0].data() {
+                                    if str.as_str() == "index" {
+                                        let index_statement =
+                                            Sql::from_str(&record.columns[4].data().display())?;
+                                        if let SerialValue::I8(num) = record.columns[3].data() {
+                                            db.read_index(
+                                                *num as usize,
+                                                &index_statement,
+                                                &select_statement,
+                                                &mut rowids,
+                                            );
+                                        }
+                                        continue;
                                     }
                                 }
-                                _ => {}
+
+                                let mut rowids: Vec<i64> = rowids.into_iter().collect();
+                                rowids.sort_unstable();
+
+                                match record.columns[2].data() {
+                                    SerialValue::String(str) => match str.as_str() {
+                                        "sqlite_sequence" => {
+                                            continue;
+                                        }
+                                        t_name => {
+                                            if select_statement.tbl_name == t_name {
+                                                match record.columns[3].data() {
+                                                    SerialValue::I8(num) => {
+                                                        let create_statement = Sql::from_str(
+                                                            &record.columns[4].data().display(),
+                                                        )?;
+
+                                                        let fields = select_statement
+                                                            .get_fields(&create_statement);
+
+                                                        let mut row_set = HashSet::new();
+                                                        let mut rowid_set = HashSet::new();
+
+                                                        if rowids.is_empty() {
+                                                            db.read_table(
+                                                                *num as usize,
+                                                                &select_statement,
+                                                                fields,
+                                                                &mut row_set,
+                                                                &mut rowid_set,
+                                                            );
+                                                        } else {
+                                                            db.read_ids_from_table(
+                                                                *num as usize,
+                                                                &select_statement,
+                                                                fields,
+                                                                &mut row_set,
+                                                                &mut rowid_set,
+                                                                &rowids,
+                                                            );
+                                                        }
+
+                                                        row_set
+                                                            .iter()
+                                                            .for_each(|str| println!("{str}"));
+                                                    }
+                                                    _ => {}
+                                                }
+                                            }
+                                        }
+                                    },
+                                    _ => {}
+                                }
                             }
                         }
                     }
